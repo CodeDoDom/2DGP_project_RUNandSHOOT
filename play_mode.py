cfde6fca751b0f15ba30f0e079ac76b7774c456d
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2023-11-30 02:36:50 +0900

    이전 저장소 코드 추가.
    저장소를 새로 만듦. 이전 저장소 코드와 내용 같음. 화면 출력 후 Runner 출력.

---
+++ play_mode.py
@@ -0,0 +1,46 @@
+from pico2d import *
+import game_framework
+
+import game_world
+from runner import Runner
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+def init():
+    global runner
+
+    running = True
+
+    runner = Runner()
+    game_world.add_object(runner, 0)
+
+
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update():
+    game_world.update()
+    # delay(0.1)
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
---
+++ runner.py
@@ -0,0 +1,11 @@
+from pico2d import *
+
+class Runner:
+    def __init__(self):
+        self.image = load_image('running.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300)
