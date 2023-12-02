9773c4d459fee3edf39f22115e4df525344bc044
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2023-12-02 09:25:45 +0900

    장애물 이동 구현.
    수정 필요.

--- hurdle.py
+++ hurdle.py
@@ -4,11 +4,17 @@ from pico2d import *
 class Hurdle:
     # image = None
     def __init__(self):
+        self.x, self.y = 720, 160 # 이 y 좌표로 하면 캐릭터 점프 위치랑  완벽하게 맞음
+        self.w, self.h = 128, 128
         self.image = load_image('drum.png')
+        # self.x, self.y = 120, 110
+        # self.image = load_image('drum_new.png')
 
     def update(self):
-        pass
+        self.x -= 1
+        # drum을 움직일 때, 3초 후에 runner이랑 동시에 움직여야 함.
+        # drum의 statemachine을 만들어야 할까?
 
     def draw(self):
-        self.image.draw(120, 160) # 이 좌표로 하면 캐릭터 점프 위치랑 완벽하게 맞음
-        
+        self.image.clip_draw(0, 0, self.w, self.h, self.x, self.y)
+        # self.image.clip_draw(0, 0, 35, 30, self.x, self.y)
