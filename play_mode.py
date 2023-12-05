0e9cee60f49cee58c802b4e4045133dd929d8260
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2023-12-06 01:04:53 +0900

    server.py 관련해 수정.

--- play_mode.py
+++ play_mode.py
@@ -4,6 +4,7 @@ from pico2d import *
 import game_framework
 
 import game_world
+import server
 from background import Background
 from drum import Drum
 from runner import Runner, Idle
@@ -12,6 +13,7 @@ from target_X import TargetX
 
 
 def handle_events():
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -19,11 +21,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         else:
-            runner.handle_event(event)
+            server.runner.handle_event(event)
 
 
 def init():
-    global runner
+#    global runner
     global background
     global wait_time
 
@@ -31,13 +33,13 @@ def init():
 
     wait_time = get_time()
 
-    # background = Background()
-    # game_world.add_object(background, 0)
+    server.background = Background()
+    game_world.add_object(server.background, 0)
 
-    runner = Runner()
-    game_world.add_object(runner, 2)
+    server.runner = Runner()
+    game_world.add_object(server.runner, 2)
 
-    game_world.add_collision_pair('runner:drum', runner, None)
+    game_world.add_collision_pair('runner:drum', server.runner, None)
 
 
 def finish():
@@ -52,7 +54,7 @@ def update():
     global targetO
     global targetX
 
-    if runner.state_machine.cur_state != Idle:
+    if server.runner.state_machine.cur_state != Idle:
         if get_time() - wait_time > random.randint(1, 3):
             targetO = TargetO()
             targetO.y -= random.randint(-100, 100)
@@ -65,7 +67,7 @@ def update():
             game_world.add_object(targetX, 1)
             wait_time = get_time()
 
-    if get_time() - wait_time > (random.random() * 100) + 0.3 and runner.state_machine.cur_state != Idle:
+    if get_time() - wait_time > (random.random() * 100) + 0.3 and server.runner.state_machine.cur_state != Idle:
         drum = Drum()
         game_world.add_collision_pair('runner:drum', None, drum)
         game_world.add_object(drum, 1)
@@ -82,10 +84,6 @@ def update():
     #         drum = Drum()
     #         game_world.add_object(drum, 1)
     #         wait_time = get_time()
-
-
-    # drum = Drum()
-    # game_world.add_object(drum, 1)
 
     game_world.update()
 
