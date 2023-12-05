a733bcd1ee3dc0bdfdb9085ab7dd460fe0e2fb1e
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2023-12-06 01:07:36 +0900

    background 시차 스크롤링 구현.

--- background.py
+++ background.py
@@ -3,13 +3,74 @@ from pico2d import *
 
 class Background:
     def __init__(self):
-        self.x, self.y = 290, 360
-        self.w, self.h = 294, 412
-        self.image = load_image('background_01.png')
+        self.x1, self.y1 = 640, 360
+
+        self.x2, self.y2 = 0, 360
+        self.x2_copy, self.y2_copy = 1280, 360
+
+        self.x3, self.y3 = 0, 360
+        self.x3_copy, self.y3_copy = 1280, 360
+
+        self.x4, self.y4 = 0, 360
+        self.x4_copy, self.y4_copy = 1280, 360
+
+        self.x5, self.y5 = 0, 360
+        self.x5_copy, self.y5_copy = 1280, 360
+
+        #self.w, self.h = 576, 324
+
+        self.image1 = load_image('sky_1.png')
+
+        self.image2 = load_image('sky_2.png')
+        self.image2_copy = load_image('sky_2.png')
+
+        self.image3 = load_image('sky_3.png')
+        self.image3_copy = load_image('sky_3.png')
+
+        self.image4 = load_image('sky_4.png')
+        self.image4_copy = load_image('sky_4.png')
+
+        self.image5 = load_image('sky_5.png')
+        self.image5_copy = load_image('sky_5.png')
 
     def update(self):
-        pass
+        self.x2 -= 1
+        self.x3 -= 1
+        self.x4 -= 3
+        self.x5 -= 2
+
+        self.x2_copy -= 1
+        self.x3_copy -= 1
+        self.x4_copy -= 3
+        self.x5_copy -= 2
 
     def draw(self):
-        # self.image.draw(600, 300)
-        self.image.clip_draw(0, 0, self.w, self.h, self.x, self.y, self.w * 2, self.h * 2)
+        self.image1.draw(self.x1, self.y1)
+
+#        if self.x2 == -640:
+#            self.x2 = 1280 + 640
+#        self.image2.draw(self.x2, self.y2)
+#        if self.x2_copy == -640:
+#            self.x2_copy = 1280 + 640
+#        self.image2_copy.draw(self.x2_copy, self.y2_copy)
+
+        if self.x3 == -640:
+            self.x3 = 1280 + 640
+        self.image3.draw(self.x3, self.y3)
+        if self.x3_copy == -640:
+            self.x3_copy = 1280 + 640
+        self.image3_copy.draw(self.x3_copy, self.y3_copy)
+
+#        if self.x4 == -640:
+#            self.x4 = 1280 + 640
+#        self.image4.draw(self.x4, self.y4)
+#        if self.x4_copy == -640:
+#            self.x4_copy = 1280 + 640
+#        self.image4_copy.draw(self.x4_copy, self.y4_copy)
+
+        if self.x5 == -640:
+            self.x5 = 1280 + 640
+        self.image5.draw(self.x5, self.y5)
+        if self.x5_copy == -640:
+            self.x5_copy = 1280 + 640
+        self.image5_copy.draw(self.x5_copy, self.y5_copy)
