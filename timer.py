982106f779fb39f6d02996f4baf63dca196f4bfc
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2023-12-06 16:34:38 +0900

    (임시)기타 추가 수정.

--- timer.py
+++ timer.py
@@ -1,5 +1,8 @@
 from pico2d import *
 
+import game_framework
+import logo_mode
+
 
 class Timer:
     def __init__(self):
@@ -12,6 +15,9 @@ class Timer:
         if get_time() - self.wait_time > 1.0:
             self.sec -= 1
             self.wait_time = get_time()
+        if (self.sec // 60) < 0.0:
+            game_framework.change_mode(logo_mode)
+
 
     def draw(self):
         self.font.draw(self.x, self.y, f'{int(self.sec // 60)}:{self.sec % 60}', (255, 255, 255))
